bcd067a56b5ca9d8ccc3eedf30d660684a6accd1
slawo <slawekpettke@gmail.com>
2022-08-29 11:38:46 +0200

    @Component, @RequwstMapping

--- src/main/java/pl/slawek/springio/wizytownik/BusinesCardController.java
+++ src/main/java/pl/slawek/springio/wizytownik/BusinesCardController.java
@@ -2,6 +2,7 @@ package pl.slawek.springio.wizytownik;
 
 
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BusinesCardController {
 
     @GetMapping("details")
-    public String getPerson(){
+    public String getPerson(Model model){ // obiekt model to spring, mvc
+        model.addAttribute("name", "Andrzej");//to zwracamy na formatke
+        model.addAttribute("surname", "Kowalski");
+        model.addAttribute("email", "andrzej.kowalpsi@gmail.com");
+        model.addAttribute("telephone", "5554443322");
         return "person details";
     }
 }
