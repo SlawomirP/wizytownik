6bb2eee530fdf2c9b226c2485387aee1883476f1
slawo <slawekpettke@gmail.com>
2022-08-29 10:13:56 +0200

    @Component, @RequwstMapping

---
+++ src/main/java/pl/slawek/springio/wizytownik/BusinesCardController.java
@@ -0,0 +1,10 @@
+package pl.slawek.springio.wizytownik;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Component
+@RequestMapping("person")
+public class BusinesCardController {
+}
